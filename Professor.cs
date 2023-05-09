666498e5b62c811210fd85ad8420cf644b5c740d
Joel Southall <southallj@my.lanecc.edu>
2023-05-09 02:59:44 -0700

    Update Profe fully

---
+++ Professor.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Professor
+{
+    // Attributes --  underscore prefix is used here to indicate that these variables are private fields
+    private string _lNumber;
+    private string _firstName;
+    private string _lastName;
+    private string _department;
+
+    // Properties
+    public string LNumber
+    {
+        get { return _lNumber; }
+        set
+        {
+            while (true)
+            {
+                // Use regex to validate the LNumber format L########
+                if (Regex.IsMatch(value, @"^L\d{8}$"))
+                {
+                    _lNumber = value;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid LNumber. LNumber must be in the format L########. Please try again:");
+                    value = Console.ReadLine();
+                }
+            }
+        }
+    }
+
+    public string FirstName
+    {
+        get { return _firstName; }
+        set { _firstName = value; }
+    }
+
+    public string LastName
+    {
+        get { return _lastName; }
+        set { _lastName = value; }
+    }
+
+    public string Department
+    {
+        get { return _department; }
+        set { _department = value; }
+    }
+
+    // Constructors
+    public Professor()
+    {
+        // Default constructor
+    }
+
+    public Professor(string lNumber, string firstName, string lastName, string department)
+    {
+        LNumber = lNumber;
+        FirstName = firstName;
+        LastName = lastName;
+        Department = department;
+    }
+
+    // Methods
+    public override string ToString()
+    {
+        return $"Name: {FirstName} {LastName}\nLNumber: {LNumber}\nDepartment: {Department}";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        do
+        {
+            // Prompt the user for input
+            Console.WriteLine("Enter the professor's LNumber:");
+            string lNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter the professor's first name:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter the professor's last name:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter the professor's department:");
+            string department = Console.ReadLine();
+
+            // Create a new Professor object with the user's input
+            Professor p = new Professor(lNumber, firstName, lastName, department);
+
+            // Print out the Professor object's information
+            Console.WriteLine(p.ToString());
+
+            // Ask the user if they want to play again
+            Console.WriteLine("Do you want to enter another Professor? (Y/N)");
+            string playAgain = Console.ReadLine().ToUpper();
+
+            if (playAgain != "Y")
+            {
+                break;
+            }
+        } while (true);
+    }
+}
